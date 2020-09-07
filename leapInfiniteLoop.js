a24270a99b554b269f29bbaaaa9e0928606d9e61
Sage Luksevish <sage.luksevish@uvm.edu>
2020-09-07 18:03:36 -0400

    D1, steps 47-48

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,15 +1,25 @@
 var controllerOptions = {};
 
 var i = 0;
+var x = (.5 * window.innerWidth);
+var y = (.5 * window.innerHeight);
+
+function draw() {
 
 Leap.loop(controllerOptions, function(frame)
 {
 
+fill(200,200,200);
+circle(x, y, 50);
+
 console.log(i);
 
-i++
+i++;
+
 }
 );
+}
+
 
 
 

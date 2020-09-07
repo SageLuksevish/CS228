901aa482cbab350edfc571a12a868387b7f43b0c
Sage Luksevish <sage.luksevish@uvm.edu>
2020-09-07 18:30:23 -0400

    D1, step 49

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -3,13 +3,24 @@ var controllerOptions = {};
 var i = 0;
 var x = (.5 * window.innerWidth);
 var y = (.5 * window.innerHeight);
+var r = 0;
+
 
 function draw() {
+fill(233,197,28);
+}
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 
 Leap.loop(controllerOptions, function(frame)
 {
 
-fill(200,200,200);
+r = getRndInteger(-1,1);
+
+x += r;
+
 circle(x, y, 50);
 
 console.log(i);
@@ -18,7 +29,7 @@ i++;
 
 }
 );
-}
+
 
 
 

8e3c108aeb229904d65aaecb98670d9785c27efb
Sage Luksevish <sage.luksevish@uvm.edu>
2020-09-08 00:14:17 -0400

    D1, step 65-73

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,8 +1,12 @@
 var controllerOptions = {};
 
-var x = (.5 * window.innerWidth);
-var y = (.5 * window.innerHeight);
-var r = 0;
+var x = 0;
+var y = 0;
+var z = 0;
+var rawXMin = 1000000;
+var rawXMax = -1000000;
+var rawYMin = 1000000;
+var rawYMax = -1000000;
 
 
 function draw() {
@@ -34,17 +38,46 @@ var curFingerName = nameMap[curFingerType];
 //var fingerName = nameMap[n];
 
 if (curFingerName == "index"){
-console.log(curFinger);
+console.log(curFinger.tipPosition);
+
+x = curFinger.tipPosition[0];
+y = curFinger.tipPosition[1];
+z = curFinger.tipPosition[2];
+
+y *= -window.innerHeight;
+
+if(x < rawXMin) {
+rawXMin =x;
+}
+
+if(x > rawXMax) {
+rawXMax =x;
+}
+
+if(y < rawYMin) {
+rawYMin =y;
+}
+
+if(y > rawYMax) {
+rawYMax =y;
+}
+
+newX = ((x - rawXMin) / (rawXMax - rawXMin)) * (window.innerWidth)
+
+newY = ((y - rawYMin) / (rawYMax - rawYMin)) * (window.innerWidth)
+
+circle(newX,newY,100);
 }
 }
 
 
-function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
+//function getRndInteger(min, max) {
+ // return Math.floor(Math.random() * (max - min + 1) ) + min;
+//}
 
 Leap.loop(controllerOptions, function(frame)
 {
+clear();
 handleFrame(frame);
 }
 );

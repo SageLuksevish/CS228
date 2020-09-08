5cab697a4ed4f8fa1fac653487a0fda40484c918
Sage Luksevish <sage.luksevish@uvm.edu>
2020-09-07 23:34:52 -0400

    D1, steps 63-65

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -9,50 +9,43 @@ function draw() {
 fill(233,197,28);
 }
 
+function handleFrame(frame){
+if (frame.hands.length == 1){
+
+var hand = frame.hands[0];
+handleHand(hand);
+}
+}
+
+function handleHand(hand){
+var fingers = hand.fingers;
+
+for (var n = 0; n < fingers.length; n++){
+handleFinger(fingers[n]);
+}
+}
+
+function handleFinger(nxtFinger){
+var curFinger = nxtFinger;
+var curFingerType = curFinger.type;
+
+var nameMap = ["thumb", "index", "middle", "ring", "pinky"];
+var curFingerName = nameMap[curFingerType];
+//var fingerName = nameMap[n];
+
+if (curFingerName == "index"){
+console.log(curFinger);
+}
+}
+
+
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 Leap.loop(controllerOptions, function(frame)
 {
-if (frame.hands.length == 1){
-
-var hand = frame.hands[0];
-
-var fingers = hand.fingers;
-
-for (var n = 0; n < fingers.length; n++){
-
-var currentFinger = fingers[n];
-var currentFingerType = currentFinger.type;
-
-var nameMap = ["thumb", "index", "middle", "ring", "pinky"];
-var currentFingerName = nameMap[currentFingerType];
-var fingerName = nameMap[n];
-
-if (currentFingerName == "index"){
-console.log(currentFinger);
-}
-
-//console.log(fingers[n]);
-
-//console.log(fingers);
-}
-
-//console.log(fingers);
-//console.log(frame.hands[0]);
-}
-
-//clear();
-
-//r = getRndInteger(-1,1);
-//s = getRndInteger(-1,1);
-
-//x += r;
-//y += s;
-
-//circle(x, y, 50);
-
+handleFrame(frame);
 }
 );
 

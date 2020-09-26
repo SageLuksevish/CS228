e7999609a54047c614c8f7786c6497524066961c
Sage Luksevish <sage.luksevish@uvm.edu>
2020-09-25 22:11:01 -0400

    D4, worked in js on train and test functions

--- Predict.js
+++ Predict.js
@@ -1,8 +1,27 @@
+var trainingCompleted = false;
+
 
 function draw() {
     clear();
 
+    if (trainingCompleted == false){
+
+        train();
+    }
+
+    test();
+
 }
 
 
+function train(){
 
+console.log("I am being trained.");
+
+trainingCompleted = true;
+}
+
+function test(){
+
+console.log("I am being tested.");
+}

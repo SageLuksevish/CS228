131b3eb276bd34ffbbda253b420d386554cb0bb8
Sage Luksevish <sage.luksevish@uvm.edu>
2020-11-03 01:41:40 -0500

    D7, 35-38

--- prepareToDraw.js
+++ prepareToDraw.js
@@ -1,8 +1,20 @@
 var img;
+var up;
+var down;
+var forward;
+var back;
+var left;
+var right;
 
 function setup() {
     createCanvas(window.innerWidth,window.innerHeight);
 
     img = loadImage('WaveHand.jpg');
+    up = loadImage('GoUp.jpg');
+    down = loadImage('GoDown.jpg');
+    forward = loadImage('GoForward.jpg');
+    back = loadImage('GoBack.jpg');
+    left = loadImage('GoLeft.jpg');
+    right = loadImage('GoRight.jpg');
 }
 
